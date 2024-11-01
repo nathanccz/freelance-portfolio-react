040d42c9fed6027415357b26170b14ac469686d3
Nathan Casarez <n.casarez@icloud.com>
2024-11-01 14:40:17 -0700

    Small copy changes to the About and Process sections.

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -15,7 +15,7 @@ const Process = ({ data }) => {
                   <div className="container">
                     <SectionHeading title="Timeline for Success" subTitle="My Process" />
                     <div className="row gy-4">
-                <p class="opacity-90">Here's how we'll turn your vision into reality:</p>
+                <p data-aos="fade-up" data-aos-duration="800" data-aos-delay="500"><strong>Here's how we'll turn your vision into reality:</strong></p>
 
                 <div class="container">
                   <div class="row">
@@ -34,7 +34,7 @@ const Process = ({ data }) => {
                                 <span><Icon icon="fa6-solid:handshake"/></span>
                               </h5>
                               <h4 class="ezy__howitworks11-title fw-bold mb-3">Set Up a Meeting</h4>
-                              <p class="opacity-75">
+                              <p>
                                   We'll set up a time to talk about your project over the phone or on a video call. During this phase, you have the chance to dream big: <strong>What does success look like to you?</strong> Let's discuss your desired goals, your target audience, design needs and budget.
                               </p>
                             </div>
@@ -50,7 +50,7 @@ const Process = ({ data }) => {
                                 <span><Icon icon="teenyicons:pdf-solid"/></span>
                               </h5>
                               <h4 class="ezy__howitworks11-title fw-bold mb-3">Receive Proposal</h4>
-                              <p class="opacity-75">
+                              <p>
                                   After our meeting, I'll put together a proposal of options available within your ballpark budget. Each option will outline the services you can expect, the timeline for completion, and the price. You'll also receive a <strong>contract for your review.</strong>
                               </p>
                             </div>
@@ -66,7 +66,7 @@ const Process = ({ data }) => {
                                 <span><Icon icon="ph:layout-fill"/></span>
                               </h5>
                               <h4 class="ezy__howitworks11-title fw-bold mb-3">Iterate on Layout</h4>
-                              <p class="opacity-75">
+                              <p>
                                 Once the contract is signed, I'll get to work. We'll set up weekly meetings to decide on layout, design and content. You'll receive three templates or wireframes to choose from. Then, we'll iterate on your chosen layout until you're satisfied, <strong>up to three revisions</strong>. I'll also start setting up your domain.
                               </p>
                             </div>
@@ -82,7 +82,7 @@ const Process = ({ data }) => {
                                 <span><Icon icon="icon-park-outline:code-computer"/></span>
                               </h5>
                               <h4 class="ezy__howitworks11-title fw-bold mb-3">Build & Test</h4>
-                              <p class="opacity-75">
+                              <p>
                                   During this phase, I'll build and test your website so that it looks good and performs well across a range of devices, browsers and display sizes. You'll be invited to test as often as you'd like. Once the site is approved, it will go live. You'll receive all assets, plus <strong>full control over the domain</strong>.
                               </p>
                             </div>
